700edd9c502cbb929e050e002719484011809662
Kushagra Kumar <79048366+kush551@users.noreply.github.com>
2024-02-23 22:39:47 +0530

    Base done 1

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
 #importing the required libraries
-import selenium
 import time
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+
+# Create an ActionChains object
+
 
 #Opening the browser
 driver = webdriver.Chrome()
+action = ActionChains(driver)
 driver.get("https://www.instagram.com/")
 time.sleep(2)
+email = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+email.send_keys("kushagrakk2008@outlook.com")
+password = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
+password.send_keys("Sudhir1!Shalini")
+loginbutton = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]')
+loginbutton.click()
+time.sleep(5)
+driver.back()
+time.sleep(5)
 
-#Entering the username and password
-username = driver.find_element(By.NAME, "kushagrakk2008@outlook.com")
-username.send_keys("username")
-password = driver.find_element(By.NAME, "Sudhir1!Shalini")
-password.send_keys("password")
+Notification= driver.find_element(By.XPATH, "//*[text()='Not Now']")
+Notification.click()
 
-#Clicking the login button
-login = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button')
-login.click()
+time.sleep(2)
+reel = driver.find_element(By.XPATH, '//*[@aria-label="Reels"]')
+reel.click()
 time.sleep(2)
 
-#Clicking the not now button
-not_now = driver.find_element(By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button')
-not_now.click()
-time.sleep(2)
+while True:
+    # Perform the action
+    time.sleep(5)
+    send_real = driver.find_element(By.XPATH,'//*[@id="mount_0_0_UE"]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div[3]/div/div/svg/polygon')
+    send_real.click()
 
-#Clicking the not now button
-not_now = driver.find_element(By.XPATH, '/html/body/div[4]/div/div/div/div[3]/button[2]')
-not_now.click()
+    action.send_keys(Keys.DOWN).perform()
 
-#Open the real tab
-real = driver.find_element(By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[5]/a')
-real.click()
+    # Wait for 5 seconds
+    time.sleep(20)
+
+
+
+
+time.sleep(10000)
 
