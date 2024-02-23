4d59c5e637a445fb5e7a3a0612e105855968e150
Kushagra Kumar <79048366+kush551@users.noreply.github.com>
2024-02-23 22:49:41 +0530

    Share Button done just have to make a person selector

--- main.py
+++ main.py
@@ -5,36 +5,39 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 
-# Create an ActionChains object
-
-
 #Opening the browser
 driver = webdriver.Chrome()
 action = ActionChains(driver)
 driver.get("https://www.instagram.com/")
 time.sleep(2)
+
+#Logging in
 email = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
 email.send_keys("kushagrakk2008@outlook.com")
 password = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
 password.send_keys("Sudhir1!Shalini")
-loginbutton = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]')
+loginbutton= driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]')
 loginbutton.click()
 time.sleep(5)
+
+#Removing the notification
 driver.back()
-time.sleep(5)
-
-Notification= driver.find_element(By.XPATH, "//*[text()='Not Now']")
-Notification.click()
-
 time.sleep(2)
+Notification = driver.find_element(By.XPATH, "//*[text()='Not Now']")
+Notification.click()
+time.sleep(2)
+
+#Clicking on the reels
 reel = driver.find_element(By.XPATH, '//*[@aria-label="Reels"]')
 reel.click()
 time.sleep(2)
 
+
+#creating a loop to keep scrolling and keep sharing the reels
 while True:
     # Perform the action
     time.sleep(5)
-    send_real = driver.find_element(By.XPATH,'//*[@id="mount_0_0_UE"]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div[1]/div/div[2]/div[3]/div/div/svg/polygon')
+    send_real = driver.find_element(By.CSS_SELECTOR, 'svg[aria-label="Direct"].x1lliihq.x1n2onr6.xyb1xck')
     send_real.click()
 
     action.send_keys(Keys.DOWN).perform()
